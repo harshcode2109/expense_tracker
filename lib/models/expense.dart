42deb3d52c529f144c4d85efef7723e17458171f
harshcode2109 <156992122+harshcode2109@users.noreply.github.com>
2025-07-23 01:54:51 +0530

    Added Hive setup and generated expense.g.dart

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,14 +1,39 @@
 // lib/models/expense.dart
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
-enum Category { food, travel, shopping, bills, other }
+part 'expense.g.dart';
 
-class Expense {
+@HiveType(typeId: 0)
+enum Category {
+  @HiveField(0)
+  food,
+  @HiveField(1)
+  travel,
+  @HiveField(2)
+  shopping,
+  @HiveField(3)
+  bills,
+  @HiveField(4)
+  other,
+}
+
+@HiveType(typeId: 1)
+class Expense extends HiveObject {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final double amount;
+
+  @HiveField(3)
   final DateTime date;
+
+  @HiveField(4)
   final Category category;
 
   Expense({
